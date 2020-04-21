51464e20828d0f1d22f1d646b4f0ed9f75c80e61
IvanaJeremijic <63556041+IvanaJeremijic@users.noreply.github.com>
2020-04-21 21:11:14 +0200

    Update vrsta kuhinje.hpp

--- vrsta kuhinje.hpp
+++ vrsta kuhinje.hpp
@@ -1,13 +1,14 @@
 #ifndef VRSTA_KUHINJE_HPP_INCLUDED
 #define VRSTA_KUHINJE_HPP_INCLUDED
+#include "restoran.hpp"
 
 enum Kuhinje{italijanska, kineska, srpska, francuska, meksicka, tajlandska, mediteranska, rostilj};
 
 class Vrsta kuhinje
 {
 private:
-    bool vegeterijanska;
     Kuhinje kuhinja;
+    Restoran r;
 };
 
 #endif // VRSTA_KUHINJE_HPP_INCLUDED
