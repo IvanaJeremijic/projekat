839f8929ad2f98115576ebff89c9da9fcafd884d
IvanaJeremijic <63556041+IvanaJeremijic@users.noreply.github.com>
2020-04-21 21:11:39 +0200

    Update obrok.hpp

--- obrok.hpp
+++ obrok.hpp
@@ -1,10 +1,13 @@
 #ifndef OBROK_HPP_INCLUDED
 #define OBROK_HPP_INCLUDED
+#include "jelo.hpp"
+#include "pice.hpp"
 
 class Obrok
 {
 private:
-
+    Jelo j;
+    Pice p;
 };
 
 #endif // OBROK_HPP_INCLUDED
