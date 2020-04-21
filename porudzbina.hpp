a4a663897fa22a0b243c33a1e5d3284a73088001
IvanaJeremijic <63556041+IvanaJeremijic@users.noreply.github.com>
2020-04-21 21:11:34 +0200

    Update porudzbina.hpp

--- porudzbina.hpp
+++ porudzbina.hpp
@@ -1,10 +1,17 @@
 #ifndef PORUDZBINA_HPP_INCLUDED
 #define PORUDZBINA_HPP_INCLUDED
+#include "restoran.hpp"
+#include "meni.hpp"
+
+enum BrzinaDostave {pice, jelo, obrok};
 
 class Porudzbina
 {
 private:
-
+    int cena;
+    BrzinaDostave brzina;
+    Restoran r;
+    Meni m;
 };
 
 #endif // PORUDZBINA_HPP_INCLUDED
